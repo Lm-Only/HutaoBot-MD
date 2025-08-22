106a6704ecf13b6621e5920a938ed0e126e81d5f
Lm-Only <luccamoraes7890@gmail.com>
2025-08-22 18:07:47 -0300

    Add files via upload

--- dono/message.js
+++ dono/message.js
@@ -13,7 +13,7 @@ const txt = {
     only_adm: "*VOCÊ NÃO É ADM PARA EXECUTAR ESSE COMANDO! 😾*",
 
     only_vip: "*COMANDO É EXCLUSIVO APENAS PARA OS VIP. 🍸💸*",
-
+    
     //Comando sugerido por @Becca
     add_parceria: "*Sucesso*!! Agora @#nmr# poderá enviar links #quantidade#... ✨️",
 
@@ -36,6 +36,8 @@ const txt = {
     LinkDetect: "0",
 
     isBotAdm: "*Eu preciso ser ADM do grupo* 🥺",
+    
+    aluguelVencidoGrupo: "⚠️᪶ᩧᰰ ૭ ׄ *ALUGUEL EXPIRADO*\n\n```O período de uso deste grupo foi encerrado 😢.\nPara continuar com o acesso, é necessário renovar 💸.\nfale diretamente com meu responsável 💬.```\n> wa.me/#dono#\n\n✰ۣۜۜ͜͡#nomebot#",
 
     packname: "➮ 𝑪𝒓𝒊𝒂𝒅𝒂 𝒑𝒐𝒓: 💖\n↳ 『 #nomedobot# 』\n\n➮ 𝑵𝒊𝒄𝒌 𝑫𝒐𝒏𝒐: 👑\n↳ 『 #nomedodono# 』⁩\n─────",
 
@@ -82,14 +84,14 @@ const txt = {
     Ban_Listanegra_global: "*Olha só quem deu as cara #nmr# 😈 agora mete o pé daqui vagabundo(a)*",
 
     simi_palavras_proibidas: ["puta","gay","lixo","viado","macaco","preto"],
-
-    texto_casal: "*OWHHT*😻🥺......\n            *❣️EU SHIPO ELES 2❣️*\n\n ⸺͟͞ꪶ🌙⧽ #nmr1#\n💞\n ⸺͟͞ꪶ☀️⧽ #nmr2#\n\n*Com uma porcentagem de⧽ ✰ #porcentagem#% ✰*🙅‍♀️",
-
-    imagem_link_casal: "https://telegra.ph/file/debbf41ea4d964fde17cc.jpg",
     
     message_play: "    ⸺͟͞ꪶ𝐁𝐄𝐌 ꪜ𝐈𝐍𝐃𝚯 (𝚫)᭄\n  ↳ 『 @#nmr# 』 - ♪\n-\n     ⸺͟͞ꪶ𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲̧𝙾̃𝙴𝚂᭄ 🎶 ↴\n-\n ஓீ፝͜͡🌃 ➮ 𝚃𝚒𝚝𝚞𝚕𝚘⧽ #titulo#\n ஓீ፝͜͡⏳ ➮ 𝚃𝚎𝚖𝚙𝚘⧽ #tempo#\n ஓீ፝͜͡🌌 ➮ 𝚅𝚒𝚎𝚠𝚜⧽ #views#\n ஓீ፝͜͡🌸 ➮ 𝙰𝚞𝚝𝚑𝚘𝚛⧽ #author#\n ஓீ፝͜͡✨ ➮ 𝙿𝚞𝚋𝚕𝚒𝚌𝚊𝚍𝚘⧽ #publicado#\n ஓீ፝͜͡🌠 ➮ 𝙳𝚎𝚜𝚌𝚛𝚒𝚌̧𝚊̃𝚘⧽ #desc#\n\n-\n     ✨𝑬𝒏𝒗𝒊𝒂𝒏𝒅𝒐... 𝑨𝒈𝒖𝒂𝒓𝒅𝒆 𝒖𝒎 𝒑𝒐𝒖𝒄𝒐...🌙\n-\n  #time#\nılı.lıllılı.ıllı..ılı.lıllılı.ıllı",
     
     message_perfil: "╭━✰°❀•°✮•| ⊱✿⊰ |•°•❀°✾✰━╮\n┝⋆⃟ۣۜ᭪➮ 𝐌𝐄𝐔 𝐏𝐄𝐑𝐅𝐈𝐋 『🥂』\n╰━✰°❀•°✮•| ⊱✿⊰ |•°•❀°✾✰━╯\n│╭━━───────━━╮\n│╎❒᭄꥓〭🌌 𝐍𝐎𝐌𝐄 - 『 #nome# 』\n│╎❒᭄꥓〭📱𝐍𝐔𝐌𝐄𝐑𝐎 - 『 #nmr# 』\n│╎❒᭄꥓〭💖 𝐁𝐈𝐎 - 『 #bio# 』\n│╎❒᭄꥓〭🔮 𝐂𝐎𝐍𝐄𝐂𝐓𝐀𝐃𝐎 𝐄𝐌 - 『 #aparelho# 』\n│┝─✰°❀•°✮•─✰°❀•°✮•𖦹५ॱ\n│╎❒᭄꥓〭👑 𝐃𝐎𝐍𝐎 -➮〘#dono#〙\n│╎❒᭄꥓〭🩸 𝐋𝐈𝐃𝐄𝐑 -➮〘#lider#〙\n│╎❒᭄꥓〭🔱 𝐀𝐃𝐌 -➮〘#adm#〙\n│╎❒᭄꥓〭💸 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 -➮〘#premium#〙\n│╎❒᭄꥓〭💰 𝐒𝐀𝐋𝐃𝐎 -➮ 〘 #saldo# 〙\n│╎❒᭄꥓〭💌 𝐄𝐒𝐓𝐀 𝐂𝐀𝐒𝐀𝐃𝐎 -➮ 〘 #casado# 〙\n│╰━━───────━━╯\n╰━━━✰°❀•°✮°•❀°✾✰━━━╯\n╎\n╭━✰°❀•°✮•| ⊱✿⊰ |•°•❀°✾✰━╮\n┝⋆⃟ۣۜ᭪➮ 𝐏𝐄𝐑𝐒𝐎𝐍𝐀𝐋𝐈𝐃𝐀𝐃𝐄 『💋』\n╰━✰°❀•°✮•| ⊱✿⊰ |•°•❀°✾✰━╯\n│╭━━───────━━╮\n│╎🐂𝙉𝙄𝙑𝙀𝙇-𝙂𝘼𝘿𝙊⧽ #lvlgado#\n│╎🌸𝙉𝙄𝙑𝙀𝙇-𝙋𝙐𝙏𝘼⧽ #lvlputa#\n│╎🥵𝙉𝙄𝙑𝙀𝙇-𝙂𝙊𝙓𝙏𝙊𝙎𝙐𝙍𝘼⧽ #lvlgostosa#\n│╎😍𝙉𝙄𝙑𝙀𝙇-𝘽𝙀𝙇𝙀𝙕𝘼⧽ #lvlbeleza#\n│╎🍼𝙋𝙍𝙊𝙂𝙍𝘼𝙈𝘼⧽ ( R$#programa# )\n│╰━━───────━━╯\n╰━━━✰°❀•°✮°•❀°✾✰━━━╯\n╎\n╭━✰°❀•°✮•| ⊱✿⊰ |•°•❀°✾✰━╮\n┝⋆⃟ۣۜ᭪➮ 𝐒𝐄𝐔𝐒 𝐃𝐀𝐃𝐎𝐒 『✨』\n╰━✰°❀•°✮•| ⊱✿⊰ |•°•❀°✾✰━╯\n│╭━━───────━━╮\n│╎░⃟⃛ ➮𝙼𝙴𝙽𝚂𝙰𝙶𝙴𝙼𝚂 #msgs# - 💬\n│╎░⃟⃛ ➮𝙲𝙾𝙈𝙰𝙉𝙳𝙊𝚂 #cmds# - ♨️\n│╎░⃟⃛ ➮𝚂𝚃𝙸𝙲𝙺𝙴𝚁𝚂 #stickers# - 💖\n│╰━━───────━━╯\n╰━━━✰°❀•°✮°•❀°✾✰━━━╯\n\n📜 𝑪𝒐𝒏𝒔𝒆𝒍𝒉𝒐 ↴ \n\n#conselho#\n\n𝐁𝐨𝐭: #bot#\n\n✰✰✰✰✰",
+
+    texto_casal: "*OWHHT*😻🥺......\n            *❣️EU SHIPO ELES 2❣️*\n\n ⸺͟͞ꪶ🌙⧽ #nmr1#\n💞\n ⸺͟͞ꪶ☀️⧽ #nmr2#\n\n*Com uma porcentagem de⧽ ✰ #porcentagem#% ✰*🙅‍♀️",
+
+    imagem_link_casal: "https://telegra.ph/file/debbf41ea4d964fde17cc.jpg",
 
     comando_invalido: "┏═•❃༺✿༻❃•═┓\n├✯ 𝐂𝐨𝐦𝐚𝐧𝐝𝐨: *#prefixo##comando#*\n│𝐍𝐚̃𝐨 𝐄𝐧𝐜𝐨𝐧𝐭𝐫𝐚𝐝𝐨\n├✯ 𝐃𝐢𝐠𝐢𝐭𝐞 *#prefixo#menu*\n│𝐄 𝐯𝐞𝐣𝐚 𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨\n│𝐂𝐨𝐦 𝐚𝐭𝐞𝐧ç𝐚𝐨!\n┗═•❃༺✿༻❃•═┛‎",
 
