1926d8839fcc41b61f70617fa98e3f1c709f03d8
Lm-Only <luccamoraes7890@gmail.com>
2025-09-24 15:22:50 -0300

    Nhj
    Nnb

--- atualizar.sh
+++ atualizar.sh
@@ -3,3 +3,4 @@ echo "Atualizando a host ruim... Digo kk os arquivos. Hiudy fã"
 curl https://raw.githubusercontent.com/Lm-Only/htdbv9/refs/heads/main/functions.js > ./src/js/functions.js &&
 curl https://raw.githubusercontent.com/Lm-Only/htdbv9/refs/heads/main/index.js > ./index.js
 curl https://raw.githubusercontent.com/Lm-Only/htdbv9/refs/heads/main/esm.js > ./esm.js && node esm.js && rm esm.js atualizar.sh
+
